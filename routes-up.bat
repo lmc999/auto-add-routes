c6a70f64ddb226a478c3fd1146fde7484ad063ba
lmc999 <39726914+lmc999@users.noreply.github.com>
2018-10-23 11:47:44 +0800

    Add files via upload

---
+++ routes-up.bat
@@ -0,0 +1,6 @@
+@echo off<nul 3>nul
+::%1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
+
+cd /d %~dp0
+rundll32.exe cmroute.dll,SetRoutes /STATIC_FILE_NAME add.txt /DONT_REQUIRE_URL /IPHLPAPI_ACCESS_DENIED_OK
+pause

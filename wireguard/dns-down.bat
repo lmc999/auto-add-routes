d39af8169c3c95bf5fc6f1fa83a6a7941de430e0
lmc999 <39726914+lmc999@users.noreply.github.com>
2020-02-22 14:19:16 +0800

    Add files via upload

---
+++ wireguard/dns-down.bat
@@ -0,0 +1,6 @@
+@ECHO OFF 
+%1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
+
+taskkill /im overture-windows-amd64.exe /f
+
+ping -n 2 127.1 >nul
---
+++ wireguard/dns-up.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d %~dp0
+%1 start mshta vbscript:createobject("wscript.shell").run("""%~0"" ::",0)(window.close)&&exit
+
+start /b overture-windows-amd64.exe -c ./config.json
+
+
+

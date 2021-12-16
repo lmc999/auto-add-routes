574030caae9dd4d3cb1ac6b517320e0e685e36a0
lmc999 <39726914+lmc999@users.noreply.github.com>
2021-12-16 11:44:25 +0800

    Update ip.sh

--- cron/ip.sh
+++ cron/ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -o ./china_ip_list.txt "https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt"
-
+rm ./add.txt del.txt
 cidr_32=255.255.255.255
 cidr_31=255.255.255.254
 cidr_30=255.255.255.252

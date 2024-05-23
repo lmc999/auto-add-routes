f50ef5f1963e300dc09fde9c1249e85d4f2942d5
lmc999 <39726914+lmc999@users.noreply.github.com>
2024-05-23 17:20:01 +0800

    replace chroute source from ipip to Misaka.io

--- cron/ip.sh
+++ cron/ip.sh
@@ -33,7 +33,7 @@ cidr_3=224.0.0.0
 cidr_2=192.0.0.0
 cidr_1=128.0.0.0
 
-curl -o ./china_ip_list.txt "https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt"
+curl -s 'https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt' | sed '/^#/d' > ./china_ip_list.txt
 
 function cnip(){
 	result=$(md5sum ./china_ip_list.txt)
